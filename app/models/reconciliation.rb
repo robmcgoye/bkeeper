3b801449a62b193c710ae13e0bbc278e1fa55085
Rob McGoye <rob@mcgoye.com>
2023-09-28 11:24:47 -0400

    Fixed syntax error

--- app/models/reconciliation.rb
+++ app/models/reconciliation.rb
@@ -5,7 +5,7 @@ class Reconciliation < ApplicationRecord
   monetize :starting_balance_cents, numericality: true
   monetize :ending_balance_cents, numericality: true
   
-  validates :start_at, :end_at, presence :true
+  validates :start_at, :end_at, presence: true
   validates :start_at, comparison: { greater_than: :end_at }
 
 end
